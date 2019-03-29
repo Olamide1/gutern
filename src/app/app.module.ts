b1116132e7f0d6032987fb1e2ad564b955cf5042
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2019-03-29 19:54:26 +0000

    Created the files, added the dependencies, added semantic-ui and a component for the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {routing } from './app.routing';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, routing,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
